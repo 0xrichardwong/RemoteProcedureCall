d907e1dfaca0f39eefcb69243d4d8672245abf6a
richardwong_ <shigenkawasaki@gmail.com>
2024-06-08 23:57:08 +0900

    Update server.py

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ result = {
     "id": "-1"
 }
 
-# リクエスト処理関数
+# Request handling function
 def handle_request(function_name, params):
     if function_name in string_callable:
         func = string_callable[function_name]
@@ -27,74 +27,69 @@ def handle_request(function_name, params):
     else:
         raise ValueError(f"Function {function_name} not found")
 
-# リクエスト処理関数2
+# Request handling function 2
 def server_response(data_dict):
-    result["id"] = data_dict["id"] 
+    result["id"] = data_dict["id"]
     result["results"] = handle_request(data_dict["method"], data_dict["params"])
     return result
 
 def main():
-    # UNIXソケットをストリームモードで作成します
+    # Create a UNIX socket in stream mode
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
-    # このサーバが接続を待つUNIXソケットのパスを設定します
+    # Define the path for the UNIX socket where this server will wait for connections
     server_address = '/tmp/socket_file'
 
-    # 以前の接続が残っていた場合に備えて、サーバアドレスをアンリンク（削除）します
+    # Unlink (remove) the server address if it already exists to avoid connection issues
     try:
         os.unlink(server_address)
-    # サーバアドレスが存在しない場合、例外を無視します
     except FileNotFoundError:
         pass
 
     print('Starting up on {}'.format(server_address))
 
-    # サーバアドレスにソケットをバインド（接続）します
+    # Bind the socket to the server address
     sock.bind(server_address)
 
-    # ソケットが接続要求を待機するようにします
+    # Listen for incoming connections on the socket
     sock.listen(1)
 
-    # 無限ループでクライアントからの接続を待ち続けます
+    # Continuously wait for client connections in an infinite loop
     while True:
-        # クライアントからの接続を受け入れます
+        # Accept a connection from a client
         connection, client_address = sock.accept()
         try:
-            print('connection from', client_address)
+            print('Connection from', client_address)
 
-            # ループが始まります。これは、サーバが新しいデータを待ち続けるためのものです。
+            # Start a loop to keep the server waiting for new data
             while True:
-                # ここでサーバは接続からデータを読み込みます。
-                # 16という数字は、一度に読み込むデータの最大バイト数です。
+                # Read data from the connection
+                # The number 256 specifies the maximum amount of data to be read at once
                 data = connection.recv(256)
 
-                # 受け取ったデータはバイナリ形式なので、それを文字列に変換します。
-                # 'utf-8'は文字列のエンコーディング方式です。
-                data_str =  data.decode('utf-8')
+                # The received data is in binary format, so convert it to a string
+                # 'utf-8' is the encoding used for the string
+                data_str = data.decode('utf-8')
 
-                # 受け取ったデータを表示します。
+                # Print the received data
                 print('Received ' + data_str)
 
-                # もしデータがあれば（つまりクライアントから何かメッセージが送られてきたら）以下の処理をします。
+                # If there is data (i.e., a message from the client), process it
                 if data:
-                    # 受け取ったメッセージを処理します。
-                    data_dict = json.loads(data_str) 
+                    # Process the received message
+                    data_dict = json.loads(data_str)
                     server_response(data_dict)  # Updated to pass data_dict to server_action
                     json_result = json.dumps(result)
                     bytes_result = json_result.encode('utf-8')
 
-                    #追加
+                    # Send the processed message back to the client
                     connection.sendall(bytes_result)
-
-                    # 処理したメッセージをクライアントに送り返します。
-                    # ここでメッセージをバイナリ形式（エンコード）に戻してから送信します。
-
-                # クライアントからデータが送られてこなければ、ループを終了します。
                 else:
-                    print('no data from', client_address)
+                    # If no data is received from the client, end the loop
+                    print('No data from', client_address)
                     break
 
-        # 最終的に接続を閉じます
+        # Finally, close the connection
         finally:
             print("Closing current connection")
             connection.close()
